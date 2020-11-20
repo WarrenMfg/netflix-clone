3ed1912f2353a61b2705f1ffeddf139ea18046bb
WarrenMfg <warrenmfg@icloud.com>
2020-11-20 08:14:06 -0500

    adds jumbotron compound component

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # **Netflix Clone**
 
 ## With Compound Styled Components
+
+_This is a work in progress_
+
+![Jumbotrons](images/readme/jumbotrons.jpg)
--- src/containers/jumbotron.js
+++ src/containers/jumbotron.js
@@ -1,4 +1,22 @@
-<Jumbotron.Container>
-  <Jumbotron.Title>Hello</Jumbotron.Title>
-  <Jumbotron.SubTitle>World</Jumbotron.SubTitle>
-</Jumbotron.Container>;
+import React from 'react';
+import Jumbotron from '../components/jumbotron';
+import jumboData from '../fixtures/jumbo.json';
+
+export function JumbotronContainer() {
+  return (
+    <Jumbotron.Container>
+      {jumboData.map(item => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
+}
