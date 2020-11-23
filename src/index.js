bf3d4aceaf1e2e80ed947999d3a9e7e5be460fd8
WarrenMfg <warrenmfg@icloud.com>
2020-11-23 12:57:53 -0500

    progress on firebase setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './app';
 import { GlobalStyles } from './global-styles';
+import { FirebaseContext } from './context/firebase';
+
+const config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
+};
 
 ReactDOM.render(
   <>
